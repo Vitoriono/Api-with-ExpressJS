e47f1387e487003547781505b6f42592d427be8f
Vitoriono <korovin.vic@gmail.com>
2023-01-05 21:56:03 +0200

    Depend inj

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Express } from 'express'
 import { Server } from 'http'
+import { ExeptionFilter } from './errors/exeption.filter';
+import { ILogger } from './logger/logger.interface';
 import { LoggerService } from './logger/logger.service'
 import { UserController } from './users/users.controller';
 
@@ -7,25 +9,33 @@ export class App {
   server!: Server;
   app: Express;
   port: number;
-  logger: LoggerService;
-  userController: UserController
+  logger: ILogger;
+  userController: UserController;
+  exeptionFilter: ExeptionFilter;
 
   constructor(
-    logger: LoggerService,
-    userController: UserController
+    logger: ILogger,
+    userController: UserController,
+    exeptionFilter: ExeptionFilter
     ) {
     this.app = express();
     this.port = 8000;
     this.logger = logger;
     this.userController = userController;
+    this.exeptionFilter = exeptionFilter;
   }
 
   useRoutes() {
     this.app.use('/users', this.userController.router);
   }
 
+  useExeptionFilters() {
+    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+  }
+
   public async init(){
     this.useRoutes();
+    this.useExeptionFilters();
     this.server = this.app.listen(this.port);
     this.logger.log(`Server is started on http://localhost:${this.port}`);
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,31 @@
 import { BaseController } from "../common/base.controller";
 import { LoggerService } from "../logger/logger.service";
 import { NextFunction, Request, Response } from "express";
+import { HTTPError } from "../errors/http-error.class";
 
 export class UserController extends BaseController {
 
-  constructor(logger: LoggerService) {
-    super(logger);
-
-    this.bindRoutes([
-      { path: '/register', method: 'post', func: this.register },
-      { path: '/login', method: 'post', func: this.login }
-    ])
+  constructor(
+    logger: LoggerService
+    ) {
+      super(logger);
+      this.bindRoutes([
+        { path: '/register', method: 'post', func: this.register },
+        { path: '/login', method: 'post', func: this.login }
+      ])
   }
-    login(req: Request, res: Response, nest: NextFunction) {
-      this.ok(res, 'login');
+    login(req: Request, res: Response, next: NextFunction) {
+      // this.ok(res, 'login');
+      next(new HTTPError(401, 'fail of autorisation', 'login'));
     }
 
-    register(req: Request, res: Response, nest: NextFunction) {
+    register(req: Request, res: Response, next: NextFunction) {
       this.ok(res, 'register');
     }
 
 
 }
   
+
+
+
