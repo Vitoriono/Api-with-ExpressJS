e394d41dd2eaea92369b5e323a9c1e4e2580f20c
Vitoriono <korovin.vic@gmail.com>
2023-01-06 07:51:29 +0200

    Step to IoC

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
+import { Container } from 'inversify';
 import { App } from './app'
 import { ExeptionFilter } from './errors/exeption.filter';
+import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { ILogger } from './logger/logger.interface';
 import { LoggerService } from './logger/logger.service';
+import { TYPES } from './types';
 import { UserController } from './users/users.controller';
 
 async function bootstrap() {
-  const logger = new LoggerService()
-  const app = new App(
-    logger, 
-    new UserController(logger), 
-    new ExeptionFilter(logger)
-  );
+  // const logger = new LoggerService()
+  // const app = new App(
+  //   logger, 
+  //   new UserController(logger), 
+  //   new ExeptionFilter(logger)
+  // );
+
+  const appContainer = new Container();
+  appContainer.bind<ILogger>(TYPES.ILogger).to(LoggerService);
+  appContainer.bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+  appContainer.bind<UserController>(TYPES.UserController).to(UserController);
+  appContainer.bind<App>(TYPES.Application).to(App);
     await app.init();
 }
 
